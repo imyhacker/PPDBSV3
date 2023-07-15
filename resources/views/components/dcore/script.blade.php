e2263bfde29a6c42c4f1417b4dcb7d338d116db1
imyhacker <arifarhan1602@gmail.com>
2023-07-15 14:10:44 +0700

    Update Dapo

--- resources/views/components/dcore/script.blade.php
+++ resources/views/components/dcore/script.blade.php
@@ -76,15 +76,6 @@
             'pdfHtml5'
         ]
     });
-    $('#belum_daful').DataTable({
-        dom: 'Bfrtip',
-        buttons: [
-            'copyHtml5',
-            'excelHtml5',
-            'csvHtml5',
-            'pdfHtml5'
-        ]
-    });
     $('#dapo').DataTable({
         dom: 'Bfrtip',
         buttons: [
@@ -94,6 +85,7 @@
             'pdfHtml5'
         ]
     });
+    
     $('#data_sekolah').DataTable();
     $('#data_foto').DataTable();
     $('#data_video').DataTable();
